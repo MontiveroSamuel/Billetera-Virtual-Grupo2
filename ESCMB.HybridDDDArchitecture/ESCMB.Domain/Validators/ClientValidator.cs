350c21034125c05ff0fefc3c53b5626b03859ae4
Florivillarreal88 <maria.florencia.villarreal@mi.unc.edu.ar>
2023-09-17 21:15:50 -0300

    cambios del 17/9 se creo la clase Cliente, su validador , caso de uso Registrar Cliente y Repositorio de SQL.

---
+++ ESCMB.HybridDDDArchitecture/ESCMB.Domain/Validators/ClientValidator.cs
@@ -0,0 +1,18 @@
+﻿using Common.Domain.Validators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESCMB.Domain.Validators
+{
+    public class ClientValidator: EntityValidator<Domain.Entities.Client>
+    {
+        public ClientValidator()
+        {
+            RuleFor(x => x.Email).NotNull().NotEmpty().WithMessage(Constants.NOTNULL_OR_EMPTY);
+        }
+    }
+}
